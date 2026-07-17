7af33f4cd42d29274a4ead1a609a028ec08ffb07
Akarshana0 <pansiluakarshana0@gmail.com>
2026-07-17 12:21:52 +0530

    Add Loading component for loading state

---
+++ loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-[#0d0d0d]">
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-gray-800 border-t-cyan-400"></div>
+    </div>
+  );
+}
